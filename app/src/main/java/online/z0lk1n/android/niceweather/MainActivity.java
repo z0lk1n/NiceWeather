57343fe0b8b32fefb74b905760f809be81c3a2f9
Vitaly Zolkin <al-not@ya.ru>
2018-06-17 23:38:47 +0500

    add menu

--- app/src/main/java/online/z0lk1n/android/niceweather/MainActivity.java
+++ app/src/main/java/online/z0lk1n/android/niceweather/MainActivity.java
@@ -1,9 +1,9 @@
 package online.z0lk1n.android.niceweather;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,25 +12,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        switch (item.getItemId()) {
-                            case R.id.detailed_weather:
-                                break;
-                            case R.id.cities_list:
-                                break;
-                            case R.id.settings:
-                                break;
-                        }
-                        return true;
-                    }
-                });
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+//        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+//        bottomNavigationView.setOnNavigationItemSelectedListener(
+//                new BottomNavigationView.OnNavigationItemSelectedListener() {
+//                    @Override
+//                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                        switch (item.getItemId()) {
+//                            case R.id.detailed_weather:
+//
+//                                break;
+//                            case R.id.cities_list:
+//                                break;
+//                            case R.id.settings:
+//                                break;
+//                        }
+//                        return true;
+//                    }
+//                });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bottom_navigation_menu, menu);
+        return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.detailed_weather:
+                return true;
+            case R.id.cities_list:
+                return true;
+            case R.id.settings:
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 //    @Override
 //    public void startCitiesListFragment() {
--- app/src/main/java/online/z0lk1n/android/niceweather/fragments/SettingsFragment.java
+++ app/src/main/java/online/z0lk1n/android/niceweather/fragments/SettingsFragment.java
@@ -1,12 +1,14 @@
 package online.z0lk1n.android.niceweather.fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 
 import online.z0lk1n.android.niceweather.R;
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
     public static final String KEY_PREF_WIND_SPEED = "pref_windSpeed";
     public static final String KEY_PREF_AIR_HUMIDITY = "pref_airHumidity";
     public static final String KEY_PREF_PRESSURE = "pref_pressure";
@@ -15,5 +17,39 @@ public class SettingsFragment extends PreferenceFragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        switch (key) {
+            case KEY_PREF_WIND_SPEED:
+                Preference windSpeedPref = findPreference(key);
+                windSpeedPref.setSummary(sharedPreferences.getString(key, ""));
+                break;
+            case KEY_PREF_AIR_HUMIDITY:
+                Preference airHumidityPref = findPreference(key);
+                airHumidityPref.setSummary(sharedPreferences.getString(key, ""));
+                break;
+            case KEY_PREF_PRESSURE:
+                Preference pressurePref = findPreference(key);
+                pressurePref.setSummary(sharedPreferences.getString(key, ""));
+                break;
+            default:
+                break;
+        }
     }
 }
